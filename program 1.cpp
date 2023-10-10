063f1e0777b6520eb35d558663e3c405286be9d3
RitikaChoudhari10 <139421536+RitikaChoudhari10@users.noreply.github.com>
2023-10-10 23:25:30 +0530

    Add files via upload

---
+++ program 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() 
+
+{
+    int N;
+    cout << "Enter a number: ";
+    cin >> N;
+    cout << "You entered: " << N << endl;
+
+    return 0;
+}
